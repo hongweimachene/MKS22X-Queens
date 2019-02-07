a134b5e6878b6fcdc0c62fb68a778509a8f9d952
hongweimachene <hchen00@stuy.edu>
2019-02-07 09:53:43 -0500

    added empty methods

---
+++ QueenBoard.java
@@ -0,0 +1,18 @@
+public class QueenBoard{
+  private int[][] board;
+  private boolean addQueens(int r, int c){
+
+  }
+  private boolean removeQueen(int r, int c){
+
+  }
+  public String toString(){
+
+  }
+  public boolean solve() {
+
+  }
+  public int countSolutions() {
+    
+  }
+}
