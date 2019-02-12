081707c3343659882b77fba4bcc1ab27fa79f663
hongweimachene <hchen00@stuy.edu>
2019-02-12 02:45:30 -0500

    successfully generates a solution

--- QueenBoard.java
+++ QueenBoard.java
@@ -4,35 +4,35 @@ public class QueenBoard{
   public QueenBoard(int size) {
     board = new int[size][size];
   }
-  private String addQueens(int r, int c){
-    String f = "";
-    // if (placeUpLeft(r,c) && placeHorizon(r,c) && placeDownLeft(r,c)) {
-    //   board[r][c] = -1;
-    //   return true;
-    // }
-    // return false;
-    if (placeUpLeft(r,c)) f+="UpLeft";
-    if (placeHorizon(r,c)) f+="Horiz";
-    if (placeDownLeft(r,c)) f+="DownLeft";
+  private boolean addQueens(int r, int c){
+    // String f = "";
     if (placeUpLeft(r,c) && placeHorizon(r,c) && placeDownLeft(r,c)) {
-       board[r][c] = -1;
-     }
-    return f;
+      board[r][c] = -1;
+      return true;
+    }
+    return false;
+    // if (placeUpLeft(r,c)) f+="UpLeft";
+    // if (placeHorizon(r,c)) f+="Horiz";
+    // if (placeDownLeft(r,c)) f+="DownLeft";
+    // if (placeUpLeft(r,c) && placeHorizon(r,c) && placeDownLeft(r,c)) {
+    //    board[r][c] = -1;
+    //  }
+    // return f;
   }
 
   private boolean placeUpLeft(int r, int c) {
-    for (int i = 1; i > r; i++) {
-      if (board[i][j] == -1){
+    for (int i = 1; i <= r && i <= c; i++) {
+      if (board[r-i][c-i] == -1){
         return false;
       }
     }
     return true;
   }
-  
+
   private boolean placeDownLeft(int r, int c) {
-    for (int i = r+1; i < board.length; i++) {
-        f (board[i][j] == -1) {
-        return false;
+    for (int i = 1; i < board.length - r && i <= c; i++) {
+        if (board[r+i][c-i] == -1) {
+          return false;
       }
     }
     return true;
@@ -71,45 +71,44 @@ public class QueenBoard{
   *        true when the board is solveable, and leaves the board in a solved state
   *@throws IllegalStateException when the board starts with any non-zero value
   */
-  // public boolean solve() {
-  //   return solveR(0);
-  // }
-  //
-  // private boolean solveR(int col) {
-  //   if (col == board[0].length) {
-  //     count++;
-  //     return true;
-  //   }
-  //   for (int row = 0; row < board.length; row++) {
-  //     if (addQueens(row, col)){
-  //       if (solveR(col+1)){
-  //         count++;
-  //         return true;
-  //       }
-  //       removeQueen(row, col);
-  //     }
-  //   }
-  //   return false;
-  // }
-  //
-  //  public int countSolutions() {
-  //    return count;
-  //  }
+  public boolean solve() {
+    return solveR(0);
+  }
+
+  private boolean solveR(int col) {
+    if (col == board[0].length) {
+      return true;
+    }
+    for (int row = 0; row < board.length; row++) {
+      if (addQueens(row, col)){
+        if (solveR(col+1)){
+          return true;
+        }
+        removeQueen(row, col);
+      }
+    }
+    return false;
+  }
+
+   // public int countSolutions() {
+   // }
 
   public static void main(String[] args) {
     QueenBoard a = new QueenBoard(8);
-    // a.solve();
-    System.out.println(a.addQueens(0,0));
-    System.out.println(a.addQueens(0,1));
-    System.out.println(a.addQueens(1,1));
-    System.out.println(a.addQueens(2,1));
-    System.out.println(a.addQueens(0,2));
-    System.out.println(a.addQueens(1,2));
-    System.out.println(a.addQueens(2,2));
-    System.out.println(a.addQueens(3,2));
-    System.out.println(a.addQueens(4,2));
-    System.out.println(a.addQueens(1,3));
+    a.solve();
+    // System.out.println(a.addQueens(0,0));
+    // System.out.println(a.addQueens(0,1));
+    // System.out.println(a.addQueens(1,1));
+    // System.out.println(a.addQueens(2,1));
+    // System.out.println(a.addQueens(0,2));
+    // System.out.println(a.addQueens(1,2));
+    // System.out.println(a.addQueens(2,2));
+    // System.out.println(a.addQueens(3,2));
+    // System.out.println(a.addQueens(4,2));
+    // System.out.println(a.addQueens(1,3));
+    // System.out.println(a.addQueens(3,4));;
+    // System.out.println(a.addQueens(6,5));
     System.out.println(a);
-    //System.out.println(a.countSolutions());
+    // System.out.println(a.countSolutions());
   }
 }
